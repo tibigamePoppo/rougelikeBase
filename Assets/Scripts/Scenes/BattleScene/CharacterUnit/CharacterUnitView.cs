af9107902d6fa0e5a454b13e1899bdc4c7a4c88f
tibigamePoppo <ryosukehashimoto1@gmail.com>
2025-03-04 20:39:20 +0900

    HPゲージの素材差し替え、各シーンのカメラの名前を分かりやすいように修正

--- Assets/Scripts/Scenes/BattleScene/CharacterUnit/CharacterUnitView.cs
+++ Assets/Scripts/Scenes/BattleScene/CharacterUnit/CharacterUnitView.cs
@@ -6,6 +6,8 @@ using System;
 using UniRx.Triggers;
 using UnityEngine.UI;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 namespace Scenes.Battle.UnitCharacter
 {
@@ -13,10 +15,12 @@ namespace Scenes.Battle.UnitCharacter
     {
         [SerializeField] private Image _hpGauge;
         [SerializeField] private EffectEmitBase[] _attackEffect;
+        [SerializeField] private Transform _lookCamera;
 
         private Animator _animator;
         public Animator Animator { get { return _animator; } }
         private int _animIDSpeed;
+        private Camera sceneCamera;
 
         private Subject<Vector3> formationPoint = new Subject<Vector3>();
         public IObservable<Vector3> OnMoveFormationPoint => formationPoint;
@@ -26,10 +30,24 @@ namespace Scenes.Battle.UnitCharacter
             _animator = GetComponent<Animator>();
             _animIDSpeed = Animator.StringToHash("Speed");
 
+            Scene currentScene = gameObject.scene;
+            sceneCamera = GetCameraInScene(currentScene);
             this.UpdateAsObservable().Subscribe(_ => {
                 _animator.SetFloat(_animIDSpeed, agent.velocity.magnitude, 0.15f, Time.deltaTime);
+                LookCamera();
             }).AddTo(this);
 
+        }
+
+        private void LookCamera()
+        {
+            Vector3 direction = sceneCamera.transform.position - transform.position;
+            direction.x = 0;
+            if (direction.magnitude > 0.1f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                _lookCamera.transform.rotation = Quaternion.Euler(targetRotation.eulerAngles.x, targetRotation.eulerAngles.y, 180);
+            }
         }
 
         public void UpdateHpGauge(float fillValue)
@@ -45,6 +63,14 @@ namespace Scenes.Battle.UnitCharacter
         private void OnAttackEffect(AnimationEvent animationEvent)
         {
             //_attackEffect.Emit(Vector3.zero);
+        }
+
+        Camera GetCameraInScene(Scene scene)
+        {
+            Camera[] camerasInScene = scene.GetRootGameObjects()
+                                            .SelectMany(go => go.GetComponentsInChildren<Camera>())
+                                            .ToArray();
+            return camerasInScene.Length > 0 ? camerasInScene.First() : null;
         }
     }
 }
