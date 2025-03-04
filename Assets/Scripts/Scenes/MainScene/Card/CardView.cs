956c9e02c8d909ce263671aafe7d3d873ba8b87e
tibigamePoppo <ryosukehashimoto1@gmail.com>
2025-03-04 12:39:47 +0900

    カードパネルでのステータス表記を追加

--- Assets/Scripts/Scenes/MainScene/Card/CardView.cs
+++ Assets/Scripts/Scenes/MainScene/Card/CardView.cs
@@ -9,12 +9,20 @@ namespace Scenes.MainScene.Cards
     {
         [SerializeField] private TextMeshProUGUI _nameText;
         [SerializeField] private Image _image;
+        [SerializeField] private TextMeshProUGUI _hpText;
+        [SerializeField] private TextMeshProUGUI _attackText;
+        [SerializeField] private TextMeshProUGUI _speedText;
+        [SerializeField] private TextMeshProUGUI _rangeText;
         [SerializeField] private TextMeshProUGUI _text;
 
         public void Init(UnitStatus unit)
         {
             _nameText.text = unit.name;
             _image.sprite = unit.sprite;
+            _hpText.text = unit.hp.ToString();
+            _attackText.text = unit.attack.ToString();
+            _speedText.text = unit.speed.ToString();
+            _rangeText.text = unit.attackRange.ToString();
             _text.text = unit.text;
         }
     }
