1eb559db31b9300e6469090ec764bc9823fd62b5
tibigamePoppo <ryosukehashimoto1@gmail.com>
2025-03-04 11:17:22 +0900

    弾道をDoTweenでの処理に変更した

--- Assets/Scripts/Scenes/BattleScene/CharacterUnit/Effect/ProjectileEffect.cs
+++ Assets/Scripts/Scenes/BattleScene/CharacterUnit/Effect/ProjectileEffect.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ProjectileEffect : EffectEmitBase
 {
-    [SerializeField] GameObject _prefab;
-    private const int SPEED = 3000;
+    [SerializeField] GameObject _impactParticle;
+    [SerializeField] GameObject _projectileParticle;
+    [SerializeField] GameObject _muzzleParticle;
+    private GameObject projectile;
+    private GameObject muzzle;
+    private GameObject impact;
     public override void Emit(Vector3 vector3)
     {
-        GameObject projectile = Instantiate(_prefab, transform.position, Quaternion.identity, transform);
-        projectile.transform.LookAt(vector3); //Sets the projectiles rotation to look at the point clicked
-        projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * SPEED);
+        projectile = Instantiate(_projectileParticle, transform.position, Quaternion.identity, transform);
+        muzzle = Instantiate(_muzzleParticle, transform.position, Quaternion.identity, transform);
+        Destroy(muzzle, 1.5f);
+        projectile.transform.DOMove(vector3, 0.2f).OnComplete(Impact);
+    }
+
+    private void Impact()
+    {
+        impact = Instantiate(_impactParticle, projectile.transform.position, Quaternion.identity, transform);
+        Destroy(projectile);
+        Destroy(impact,3.5f);
     }
 }
