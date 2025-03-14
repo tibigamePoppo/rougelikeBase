793cac291a0840009a0971e122353b5a29e57ec9
tibigamePoppo <ryosukehashimoto1@gmail.com>
2025-03-14 17:20:05 +0900

    ダメージポップアップの仕方の変更

--- Assets/Scripts/Scenes/BattleScene/CharacterUnit/Effect/DamagePopUp.cs
+++ Assets/Scripts/Scenes/BattleScene/CharacterUnit/Effect/DamagePopUp.cs
@@ -3,13 +3,12 @@ using DG.Tweening;
 
 public class DamagePopUp : MonoBehaviour
 {
+    private const float ANIMATIONTIME = 0.5f;
     void Start()
     {
-        transform.DOLocalMoveY(-50, 0.25f).SetDelay(0.15f).SetEase(Ease.OutCirc).OnComplete(Destroy);
-    }
-
-    public void Destroy()
-    {
-        Destroy(this.gameObject,0.1f);
+        transform.DOLocalJump(Vector3.zero, -40, 1, ANIMATIONTIME).SetEase(Ease.InOutSine);
+        transform.DOScale(transform.localScale * 1.1f, ANIMATIONTIME / 2).SetEase(Ease.InSine).SetLoops(2, LoopType.Yoyo);
+        Destroy(this.gameObject, ANIMATIONTIME + 0.1f);
+        //transform.DOLocalMoveY(-50, 0.25f).SetDelay(0.15f).SetEase(Ease.OutCirc).OnComplete(Destroy);
     }
 }
