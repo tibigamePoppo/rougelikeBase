242cf25f6c9bcdecc31fe9bc5c0b261f37ab4dc4
tibigamePoppo <ryosukehashimoto1@gmail.com>
2025-04-03 22:09:36 +0900

    陣形のパラメータの微修正

--- Assets/Scripts/Scenes/BattleScene/MouseInput/BatleInitalFormationView.cs
+++ Assets/Scripts/Scenes/BattleScene/MouseInput/BatleInitalFormationView.cs
@@ -142,7 +142,7 @@ namespace Scenes.Battle
                     clac.FormationI(_meleeUnits, _rangeUnits, path);
                     break;
                 case FormationType.FormationV:
-                    path = new Vector3[] { new Vector3(-1f, 0, 0.1f), new Vector3(0, 0, -0.5f), new Vector3(1, 0, 0.1f) };
+                    path = new Vector3[] { new Vector3(-1f, 0, 0.1f), new Vector3(0, 0, -0.4f), new Vector3(1, 0, 0.1f) };
                     path = path.Select(v => v * unitsScale).ToArray();
                     clac.FormationI(_meleeUnits, _rangeUnits, path);
                     break;
